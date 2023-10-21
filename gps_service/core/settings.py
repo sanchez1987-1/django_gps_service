6c03fec0445132313fe272cb7c63119cdf762804
sanchez1987-1 <sanchez1987-1@mail.ru>
2023-10-21 13:24:44 +0500

    correct static files

--- gps_service/core/settings.py
+++ gps_service/core/settings.py
@@ -142,7 +142,7 @@ if DEBUG:
         os.path.join(BASE_DIR, 'static')
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
