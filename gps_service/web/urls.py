61f1606504d06f548dcb929df044377fbe889210
sanchez1987-1 <sanchez1987-1@mail.ru>
2023-11-18 19:09:56 +0500

    отключил map

--- gps_service/web/urls.py
+++ gps_service/web/urls.py
@@ -7,7 +7,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('map/', gps_view, name='map'),
+    # path('map/', gps_view, name='map'),
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
